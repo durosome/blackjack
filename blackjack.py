dba14579b9c6c956318f3034e866f6748c0ec23c
soawesomesonic <durosome@gmail.com>
2021-03-19 22:18:11 +0300

    1. Rollback previously commit because we turn BUG into FEATURE - we reserved Dealer.Table.Player_Slot[0] for the DEALER, other player_slots works fine 2. Added some documentation for class Player_Slot

--- blackjack.py
+++ blackjack.py
@@ -8,15 +8,16 @@ class Dealer:
     """
 
     def __init__(self):  # initialization
+
         pass
 
     def rule_game(self, players: list):  # rule game
         self.players = players
-        self.slots_number = 7
+        self.slots_number = 7  # numbers of possible players at the table, defines from rules of the game #TODO maybe we should create class Rules
         self.table = Table(self.slots_number)
         self.slots = []
 
-        for self.slot_id in range(0, self.slots_number):
+        for self.slot_id in range(0, self.slots_number + 1): # +1, because we should reserve slot[0] for dealer
             self.table.create_slot(self.slot_id)
 
         for player_id in players:
@@ -42,25 +43,73 @@ class Table:
         self.players = []
         self.player_slots = []
 
-    def create_player(self, player_id: str):  # player takes a seat
+    def create_player(self, player_id: str):  # player enters the table and want to play
         self.players.append(Player(player_id))
 
     def create_slot(self, slot_id: int):
         self.player_slots.append(Player_Slot(slot_id))
 
+    def player_take_slot(self, player_id: str):
+        pass
+
+
 
 class Player_Slot:
+    """
+    1. we have a table
+        --  [       ]
+    2. this class adds a slots on it
+        --  [ () () ]
+    3. in each slot we store slot_id, hand, player_id
+        example:
+            column = objects
+            row = attributes of objects
+
+        --  [         (0)                  (1)           ]
+            [    (['4♣', 'K♦'])      (['10♣', 'A♦'])    ]
+            [       (dealer)        (soawesomesonic)    ]
+
+    Dealer.Table.Player_Slot[0] is always reserved for DEALER
+
+    example usage:
+        we can get
+            * slot_id it is basically positional number of slot #TODO maybe we can delete this attribute
+            - Dealer.Table.Player_Slot[11].slot_id
+                --it will return us a player_id, looks like: '11'
+
+            * player_id of player in 7th slot on the table
+            - Dealer.Table.Player_Slot[7].player_id
+                -it will return us a player_id, looks like: 'soawesomesonic'
+
+            * hand of player in 3rd slot on the table
+            - Dealer.Table.Player_Slot[3].player_hand
+                -it will return us a hand, looks like: ['4♣', 'K♦']
+
+    """
     def __init__(self, slot_id: int):
-        self.slot_id = slot_id + 1
+        self.slot_id = slot_id
+        self.player_id = ''
+        self.hand = []
+
 
 class Player:
+    """
+    the_list_of_players[0] should always be dealer
+        it's reserved slot for concrete DEALER
+
+
+    """
+
     def __init__(self, player_id):
         self.player_id = player_id
-        self.player_hand = []
 
 
-the_list_of_players = ['soawesomesonic', 'filinfilin', 'Nightcrowler28', '1', '3', '5', '88', '99']
+the_list_of_players = ['dealer', 'soawesomesonic', 'filinfilin', 'Nightcrowler28', '1', '3', '5', '88', '99']
 dealer = Dealer()
 dealer.rule_game(the_list_of_players)
-for i in range (0, len(dealer.table.player_slots)) :
+
+for i in range(0, len(dealer.table.player_slots)):
     print(i, dealer.table.player_slots[i].slot_id)
+
+for i in range(0, len(dealer.table.players)):
+    print(i, dealer.table.players[i].player_id)
